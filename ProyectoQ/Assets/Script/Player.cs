2ba793613c1bcc24d4eb76bb0d0e380099055641
Aldoyvh <77396693+Aldoyvh@users.noreply.github.com>
2022-05-02 15:52:11 -0500

    movimiento y tilemap

--- ProyectoQ/Assets/Script/Player.cs
+++ ProyectoQ/Assets/Script/Player.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     [SerializeField] private Rigidbody2D rbody2D;
     [SerializeField] private float currentspeed;
     [SerializeField] private float movementSpeed;
+
+
+    [SerializeField] SpriteRenderer sprRenderer;
+    [SerializeField] Animator anmtr;
 
 
     // Start is called before the first frame update
@@ -16,9 +21,39 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Movement();
+
+    }
     void Update()
     {
+        Inputs();
+    }
+    void Inputs()
+    {
         currentspeed = Input.GetAxis("Horizontal");
+        
+        if (currentspeed != 0)
+        {
+            if (currentspeed < 0)
+            {
+                sprRenderer.flipX = true;
+                anmtr.SetBool("isRunning", true);
+            }
+            else
+            {
+                sprRenderer.flipX = false;
+                anmtr.SetBool("isRunning", true);
+            }    
+        }
+        else
+        {
+            anmtr.SetBool("isRunning", false);
+        }
+    }
+    void Movement()
+    {
         rbody2D.velocity = currentspeed * Vector2.right * movementSpeed + rbody2D.velocity.y * Vector2.up;
     }
 }
